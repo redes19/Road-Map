ccf6884c786bee6bd772ab5599e8f3cd36987f75
Loris Laurenti <lloris19@outlook.fr>
2024-02-12 19:42:56 +0100

    scrollTriger

--- roadmap/src/Component/Quizzeo.jsx
+++ roadmap/src/Component/Quizzeo.jsx
@@ -1,10 +1,16 @@
+// import React, {useEffect, useRef} from 'react'
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+// gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 import React from 'react';
 
 export default function Quizzeo() {
 
+
   return (
     <div className='projet'>
-      <div className='txt'>
+      <div className='txt1'>
         <h1>Quizzeo</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod sequi consequuntur libero dolor nam hic, ab consectetur ea vitae dolore, sed architecto eveniet aperiam nemo beatae quaerat doloremque optio eaque!</p>
       </div>
