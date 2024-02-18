73e976c6435988aa62f5b95100138ea3760567ab
redes19 <120016236+redes19@users.noreply.github.com>
2024-02-18 18:38:47 +0100

    add animation

--- roadmap/src/Component/Quizzeo.jsx
+++ roadmap/src/Component/Quizzeo.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 
 export default function Quizzeo() {
 
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    // Animation d'apparition de la div
+    gsap.from(divRef.current, {
+      opacity: 0,
+      x: -150,
+      duration: 3,
+      delay: 1,
+      ease: "power3.out",
+    });
+  }, []);
+
   return (
     <div className='projet'>
-      <div className='txt'>
+      <div className='txt' ref={divRef}>
         <h1>Quizzeo</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod sequi consequuntur libero dolor nam hic, ab consectetur ea vitae dolore, sed architecto eveniet aperiam nemo beatae quaerat doloremque optio eaque!</p>
       </div>
